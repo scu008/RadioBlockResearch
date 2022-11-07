729c095f393ea3ff59d31769decbe959aa881ed0
scu008 <scu008@nate.com>
2022-11-07 10:29:16 +0900

    pi up

--- gr-customModule/build/include/customModule/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lws/RadioBlockResearch/gr-customModule/include/customModule
-# Build directory: /home/lws/RadioBlockResearch/gr-customModule/build/include/customModule
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
